2fa5e40b4a843c7afe4b3d0dfc5ef902f18badbf
Kyle Machulis <kyle@nonpolynomial.com>
2011-03-19 22:29:37 -0700

    Finished very basic OSC utility for emokit Added pd example

--- c/examples/emokit_osc/emokit_osc.cpp
+++ c/examples/emokit_osc/emokit_osc.cpp
@@ -2,23 +2,37 @@
     Simple example of sending an OSC message using oscpack.
 */
 
-#include <stdio.h>
-#include <string.h>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <csignal>
+#include <iostream>
 #include "oscpack/osc/OscOutboundPacketStream.h"
 #include "oscpack/ip/UdpSocket.h"
 #include "libepoc.h"
 
 #define ADDRESS "127.0.0.1"
-#define PORT 7000
+#define PORT 9997
 
-#define OUTPUT_BUFFER_SIZE 1024
+#define OUTPUT_BUFFER_SIZE 4096
+
+void sigproc(int i)
+{
+	std::cout << "closing epoc and quitting" << std::endl;
+	exit(0);
+}
 
 int main(int argc, char* argv[])
 {
+	signal(SIGINT, sigproc);
+#ifndef WIN32
+	signal(SIGQUIT, sigproc);
+#endif
+
     UdpTransmitSocket transmitSocket( IpEndpointName( ADDRESS, PORT ) );
     
     char buffer[OUTPUT_BUFFER_SIZE];
-    osc::OutboundPacketStream p( buffer, OUTPUT_BUFFER_SIZE );
+
 
 	FILE *input;
 	FILE *output;
@@ -59,18 +73,15 @@ int main(int argc, char* argv[])
 		if(epoc_read_data(d, data) > 0)
 		{
 			epoc_get_next_frame(&frame, data);
+			osc::OutboundPacketStream p( buffer, OUTPUT_BUFFER_SIZE );
 			p << osc::BeginBundleImmediate
-			  << osc::BeginMessage( "/test1" ) 
-			  << true << 23 << (float)3.1415 << "hello" << osc::EndMessage
-			  << osc::BeginMessage( "/test2" ) 
-			  << true << 24 << (float)10.8 << "world" << osc::EndMessage
+			  << osc::BeginMessage( "/epoc/channels" )
+			  << frame.F3 << frame.FC6 << frame.P7 << frame.T8 << frame.F7 << frame.F8 << frame.T7 << frame.P8 << frame.AF4 << frame.F4 << frame.AF3 << frame.O2 << frame.O1 << frame.FC5 << osc::EndMessage
+			  << osc::BeginMessage( "/epoc/gyro" ) 
+			  << frame.gyroX << frame.gyroY << osc::EndMessage
 			  << osc::EndBundle;
     
 			transmitSocket.Send( p.Data(), p.Size() );
-
-			//printf("%d %d %d %d %d\n", frame.gyroX, frame.gyroY, frame.F3, frame.FC6, frame.P7);
-		  
-			fflush(stdout);
 		}
 	}
 
