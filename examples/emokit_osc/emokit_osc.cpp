9376e1c0a21de7401b5b0dfcae1ca6c44cef790d
fundamental <mark.d.mccurry@gmail.com>
2014-09-19 16:17:02 -0400

    Update emokit_osc With New API

--- examples/emokit_osc/emokit_osc.cpp
+++ examples/emokit_osc/emokit_osc.cpp
@@ -9,10 +9,10 @@
 #include <iostream>
 #include "oscpack/osc/OscOutboundPacketStream.h"
 #include "oscpack/ip/UdpSocket.h"
-#include "libepoc.h"
+#include "emokit/emokit.h"
 
 #define ADDRESS "127.0.0.1"
-#define PORT 9997
+#define PORT 4321
 
 #define OUTPUT_BUFFER_SIZE 4096
 
@@ -20,6 +20,11 @@ void sigproc(int i)
 {
 	std::cout << "closing epoc and quitting" << std::endl;
 	exit(0);
+}
+
+float conv(int v)
+{
+    return (v-8200)/8200.0;
 }
 
 int main(int argc, char* argv[])
@@ -36,57 +41,44 @@ int main(int argc, char* argv[])
 
 	FILE *input;
 	FILE *output;
-	enum headset_type type;
   
 	char raw_frame[32];
-	struct epoc_frame frame;
-	epoc_device* d;
+	struct emokit_frame frame;
+	emokit_device* d;
 	uint8_t data[32];
-	if (argc < 2)
-	{
-		fputs("Missing argument\nExpected: epocd [consumer|research|special]\n", stderr);
-		return 1;
-	}
   
-	if(strcmp(argv[1], "research") == 0)
-		type = RESEARCH_HEADSET;
-	else if(strcmp(argv[1], "consumer") == 0)
-		type = CONSUMER_HEADSET;
-	else if(strcmp(argv[1], "special") == 0)
-		type = SPECIAL_HEADSET;
-	else {
-		fputs("Bad headset type argument\nExpected: epocd [consumer|research|special] source [dest]\n", stderr);
-		return 1;
-	}
-  
-	epoc_init(type);
-
-	d = epoc_create();
-	printf("Current epoc devices connected: %d\n", epoc_get_count(d, EPOC_VID, EPOC_PID));
-	if(epoc_open(d, EPOC_VID, EPOC_PID, 0) != 0)
+	d = emokit_create();
+	printf("Current epoc devices connected: %d\n", emokit_get_count(d, EMOKIT_VID, EMOKIT_PID));
+	if(emokit_open(d, EMOKIT_VID, EMOKIT_PID, 1) != 0)
 	{
 		printf("CANNOT CONNECT\n");
 		return 1;
 	}
 	while(1)
 	{
-		if(epoc_read_data(d, data) > 0)
+        int r;
+		if((r=emokit_read_data_timeout(d, 1000)) > 0)
 		{
-			epoc_get_next_frame(&frame, data);
+			frame = emokit_get_next_frame(d);
 			osc::OutboundPacketStream p( buffer, OUTPUT_BUFFER_SIZE );
-			p << osc::BeginBundleImmediate
-			  << osc::BeginMessage( "/epoc/channels" )
-			  << frame.F3 << frame.FC6 << frame.P7 << frame.T8 << frame.F7 << frame.F8 << frame.T7 << frame.P8 << frame.AF4 << frame.F4 << frame.AF3 << frame.O2 << frame.O1 << frame.FC5 << osc::EndMessage
-			  << osc::BeginMessage( "/epoc/gyro" ) 
-			  << frame.gyroX << frame.gyroY << osc::EndMessage
-			  << osc::EndBundle;
+			p << osc::BeginMessage( "/multiplot" )
+			  << conv(frame.F3) << conv(frame.FC6) << conv(frame.P7)
+              << conv(frame.T8) << conv(frame.F7)  << conv(frame.F8)
+              << conv(frame.T7) << conv(frame.P8)  << conv(frame.AF4)
+              << conv(frame.F4) << conv(frame.AF3) << conv(frame.O2)
+              << conv(frame.O1) << conv(frame.FC5) << osc::EndMessage;
     
 			transmitSocket.Send( p.Data(), p.Size() );
-		}
+		} else if(r == 0)
+            fprintf(stderr, "Headset Timeout\n");
+        else {
+            fprintf(stderr, "Headset Error\n");
+            break;
+        }
 	}
 
-	epoc_close(d);
-	epoc_delete(d);
+	emokit_close(d);
+	emokit_delete(d);
 	return 0;
 
 }
