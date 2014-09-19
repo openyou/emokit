bde6bf9ac21d83f4e66205ce1cb060bdd4a730e5
fundamental <mark.d.mccurry@gmail.com>
2014-09-19 16:14:28 -0400

    Use Timeout API in contact.c

--- examples/contact/contact.c
+++ examples/contact/contact.c
@@ -40,8 +40,12 @@ int main(int argc, char **argv)
 	}
 	printf("Connected to headset.\n");
 	
-	if (emokit_read_data(d)<=0) {
-		printf("Error reading from headset\n");
+	r = emokit_read_data_timeout(d,1000);
+	if (r<=0) {
+		if(r<0)
+			fprintf(stderr, "Error reading from headset\n");
+		else
+			fprintf(stderr, "Headset Timeout...\n");
 		emokit_close(d);
 		emokit_delete(d);
 		return 1;
@@ -49,11 +53,14 @@ int main(int argc, char **argv)
 
 	struct emokit_frame c;
 	while (!quit) {
-		if(emokit_read_data(d) > 0) {						
+		int err = emokit_read_data_timeout(d, 1000);
+		if(err > 0) {
 			c = emokit_get_next_frame(d);			
 			fprintf(stdout,"\033[H\033[2JPress CTRL+C to exit\n\nContact quality:\nF3  %4d\nFC6 %4d\nP7  %4d\nT8  %4d\nF7  %4d\nF8  %4d\nT7  %4d\nP8  %4d\nAF4 %4d\nF4  %4d\nAF3 %4d\nO2  %4d\nO1  %4d\nFC5 %4d",c.cq.F3, c.cq.FC6, c.cq.P7, c.cq.T8,c.cq.F7, c.cq.F8, c.cq.T7, c.cq.P8, c.cq.AF4, c.cq.F4, c.cq.AF3, c.cq.O2, c.cq.O1, c.cq.FC5);
 			fflush(stdout);
-		} 	
+		} else if(err == 0) {
+			fprintf(stderr, "Headset Timeout...\n");
+		}
 	}
 	emokit_close(d);
 	emokit_delete(d);
